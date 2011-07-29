2bb2ace3a065ea09854f1faca186bd0bd256e776
Alexey Zakharov <alexey.v.zaharov@gmail.com>
2011-07-29 18:20:16 +0600

    last

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ var redis = require('redis');
 var express = require('express');  
 var fs = require('fs');
 
+var rtgpath = "redis://witcraft:f45a034960b524b8e3fe94a728a4c5d5@bluegill.redistogo.com:9291/";
+var rtg = require("url").parse(process.env.REDISTOGO_URL || rtgpath);
+ 
+
 var app = express.createServer();
 
 app.configure(function(){
@@ -22,7 +26,7 @@ app.configure(function(){
 });
 
 app.get('/hello', function(req,res) {
-  var pub = redis.createClient(); 
+  var pub = redis.createClient(rtg.port, rtg.hostname); 
   pub.auth(rtg.auth.split(":")[1]);  
   
   pub.publish("hello", JSON.stringify({ msg : "it works"}));
@@ -67,11 +71,11 @@ io.sockets.on('connection', function (socket) {
   sub
 
   socket.on('unsubscribe', function (channel) {   
+    deleteSocketFromChannel(channel);
     sub.unsubscribe(channel); 
   });
 
   socket.on('subscribe', function (channel) {
-    channels.get
     sub.subscribe(channel);
   });
   
